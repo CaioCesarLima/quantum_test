96ea78464a2c354fb4afb962200aff9d35afb174
CaioCesarLima <cesarlimacaio@gmail.com>
2022-06-15 14:00:01 -0300

    music search by plan

--- quantum_backend/lib/quantum_backend/Music/show.ex
+++ quantum_backend/lib/quantum_backend/Music/show.ex
@@ -1,19 +1,23 @@
 defmodule QuantumBackend.Music.Show do
+  import Ecto.Query
+
   alias QuantumBackend.{Repo, Music}
   alias Ecto.UUID
+
 
   def call(id) do
     case UUID.cast(id) do
       :error -> {:error, "Invalid ID format"}
-      {:ok, _uuid} -> get()
+      {:ok, _uuid} -> get(id)
     end
   end
 
-  def get() do
-    case Repo.all(Music) do
+  def get(id) do
+    case from(m in Music, where: m.plan_id == ^id) |>  Repo.all() do
       nil -> {:error, "Musics not found"}
       musics -> {:ok, Repo.preload(musics, :plan)}
     end
   end
+
 
 end
